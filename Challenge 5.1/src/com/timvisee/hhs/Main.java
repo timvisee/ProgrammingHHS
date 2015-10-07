fd9affb9cb9f3ea56242006977bc452789738bcd
Tim Visée <timvisee@gmail.com>
2015-10-07 22:19:53 +0200

    Added base class for Challenge 5.1

---
+++ Challenge 5.1/src/com/timvisee/hhs/Main.java
@@ -0,0 +1,13 @@
+package com.timvisee.hhs;
+
+public class Main {
+
+    /**
+     * Main method called on start.
+     *
+     * @param args Startup arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}

01464ed95f95517eb10354ba1ae2e41a01851e83
Tim Visée <timvisee@gmail.com>
2015-10-07 23:42:34 +0200

    Added base class of challenge 2.9

---
+++ Challenge 2.9/src/com/timvisee/hhs/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Developed by Tim Visee, as challenges for De Haagse Hoogeschool.
+ * Copyright Tim Visee (c) 2015. All rights reserved.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
+package com.timvisee.hhs;
+
+public class Main {
+
+    /**
+     * Main method called on start.
+     *
+     * @param args Startup arguments.
+     */
+    public static void main(String[] args) { }
+}
